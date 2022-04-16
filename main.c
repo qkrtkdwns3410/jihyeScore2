4aac14269c13b3d49b85c7ef0d8e31021e0d8d57
jihye94 <qkrtkdwns3410@naver.com>
2022-04-16 20:44:53 +0900

    22/4/16

--- main.c
+++ main.c
@@ -6,6 +6,14 @@
  *
  * */
 int main() {
+    int score_list[5][3];
+    
     printf("Hello, World!\n");
+    
+    printf("학생성적을 입력 하세요 (5명의 학생 자료)\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d번(국어, 영어, 수학) : ");
+        scanf("%d %d %d",score_list[i][0],sco,)
+    }
     return 0;
 }
